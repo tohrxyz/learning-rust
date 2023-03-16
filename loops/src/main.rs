31abffa7d97fd94bb616f9984a2fda3ac88778ee
tomashrib <tomas.hrib@proton.me>
2023-03-16 11:46:58 +0100

    While loop

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,13 +1,54 @@
 fn main() {
-    let mut counter = 0;
+    let mut number = 10;
+    
+    while number != 0 {
+        println!("number: {number}");
+        number -= 1;
+    }
 
-    let result = loop {
-        counter += 1;
-
-        if counter == 10 {
-            break counter * 2;
-        }
-    };
-
-    println!("The result is {result}");
+    println!("LIFTOFF!!!");
 }
+
+
+
+// fn main() {
+//     let mut count = 0;
+    
+//     'counting_up: loop {
+//         println!("count = {count}");
+//         let mut remaining = 10;
+
+//         loop {
+//             println!("remaining = {remaining}");
+            
+//             if remaining == 9 {
+//                 break;
+//             }
+//             if count == 2 {
+//                 break 'counting_up;
+//             }
+//             remaining -= 1;
+//         }
+
+//         count += 1;
+//     }
+//     println!("End count = {count}");
+// }
+
+
+
+
+
+// fn main() {
+//     let mut counter = 0;
+
+//     let result = loop {
+//         counter += 1;
+
+//         if counter == 10 {
+//             break counter * 2;
+//         }
+//     };
+
+//     println!("The result is {result}");
+// }
