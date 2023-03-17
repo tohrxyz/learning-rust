ee13fbaba4fab40234a3199e726cf7b84a357647
tomashrib <tomas.hrib@proton.me>
2023-03-17 18:13:41 +0100

    Improved readability for temperature_converter

--- temperature_converter/src/main.rs
+++ temperature_converter/src/main.rs
@@ -3,18 +3,21 @@ use std::io;
 fn main(){
 
     let mut input: String = String::new();
+
     loop {
         println!("Enter Fahrenheit (F) or Celsius (C)? :");
 
         io::stdin()
             .read_line(&mut input)
             .expect("Failed to read input.");
-        
+    
         if input.trim() != "F" && input.trim() != "C" {
+            
             input.clear();
             continue;
         }
         else {
+            
             input = input.trim().to_string();
             break;
         }
@@ -24,6 +27,7 @@ fn main(){
     let degree_value: f64;
 
     loop {
+        
         println!("Enter value for {}: ", &input);
 
         io::stdin()
@@ -44,27 +48,21 @@ fn main(){
 
     let result = convert_f_c(&input, degree_value);
 
-    println!("Result: {}{} = {:.2}{}", degree_value, &input, result, check_which_unit(&input));
-
-    
+    println!("Result: {}˚{} = {:.2}°{}", degree_value, &input, result, check_which_unit(&input));
 }
 
 fn convert_f_c(unit: &str, value: f64) -> f64 {
-    if unit == "F" {
-        return (value - 32.0) * 5.0/9.0;
-    } 
-    if unit == "C" {
-        return (value * (9.0/5.0)) + 32.0;
-    }
-    else {
-        return 0.0;
-    }
+    
+    if unit == "F" { (value - 32.0) * 5.0/9.0 }
+
+    else if unit == "C" { (value * (9.0/5.0)) + 32.0 }
+
+    else { 0.0 }
 }
 
 fn check_which_unit(unit: &str) -> String{
-    if unit == "F" {
-        return "C".to_string();
-    } else {
-        return "F".to_string();
-    }
+    
+    if unit == "F" { "C".to_string() } 
+
+    else { "F".to_string() }
 }
