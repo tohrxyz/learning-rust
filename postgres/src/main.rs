38df2160de36ceb360b5e78bcc50c907fff902e2
tomashrib <tomas.hrib@proton.me>
2023-05-14 22:24:32 +0200

    feat: add proper error handling

--- postgres/src/main.rs
+++ postgres/src/main.rs
@@ -1,57 +1,77 @@
 use tokio_postgres::NoTls;
 use std::io::{self, Write};
 
+struct Login {
+    username: String,
+    password: String,
+    db_name: String,
+}
+
 #[tokio::main]
 async fn main() {
 
     println!("[Logging into PostgreSQL server]");
-    let username = input("Enter username: ").unwrap();
-    let password = rpassword::read_password_from_tty(Some("Enter password: ")).unwrap();
-    let db_name = input("Enter db name: ").unwrap();
-
-    let login_credentials = format!("host=localhost user={} password={} dbname={}", &username, &password, &db_name);
-
-    // Connect to the database
-    let (client, connection) =
-        tokio_postgres::connect(&login_credentials, NoTls)
-        .await.unwrap()
-    ;
-
-    // The connection object performs the actual communication with the database,
-    // so spawn it off to run on its own.
-    tokio::spawn(async move {
-        if let Err(e) = connection.await {
-            eprintln!("connection error: {}", e);
-        }
-    });
-
-    println!("[Connected successfully to {}]", &db_name);
-
+    
+    let mut login = Login {
+        username: String::new(),
+        password: String::new(),
+        db_name: String::new(),
+    };
+    
     loop {
-        let name = input("Please enter name: ").unwrap();
-        let email = input("Please enter email: ").unwrap();
+        login.username = input("Enter username: ").unwrap();
+        login.password = rpassword::read_password_from_tty(Some("Enter password: ")).unwrap();
+        login.db_name = input("Enter db name: ").unwrap();
     
-        // Insert a new user
-        client.execute(
-            "INSERT INTO users (name, email) VALUES ($1, $2)",
-            &[&name, &email],
-        ).await.unwrap();
+        let login_credentials = format!("host=localhost user={} password={} dbname={}", &login.username, &login.password, &login.db_name);
     
-        println!("[Inserted new user]");
+        let connection_result = tokio_postgres::connect(&login_credentials, NoTls).await;
+        
+        match connection_result {
+            Ok((client, connection)) => {
+                tokio::spawn(async move {
+                    if let Err(e) = connection.await {
+                        eprintln!("connection error: {}", e);
+                    }
+                });
+                
+                println!("[Connected successfully to {}]", &login.db_name);
 
-        let add_another_user = input("Add another user? [y/n]: ").unwrap();
+                loop {
+                    let name = input("Please enter name: ").unwrap();
+                    let email = input("Please enter email: ").unwrap();
+                
+                    // Insert a new user
+                    client.execute(
+                        "INSERT INTO users (name, email) VALUES ($1, $2)",
+                        &[&name, &email],
+                    ).await.unwrap();
+                
+                    println!("[Inserted new user]");
 
-        match add_another_user.trim().to_lowercase().as_str() {
-            "y" => {
-                println!("[Continuing...]");
-                continue;
-            }
-            _ => {
-                println!("[Breaking...]");
+                    let add_another_user = input("Add another user? [y/n]: ").unwrap();
+
+                    match add_another_user.trim().to_lowercase().as_str() {
+                        "y" => {
+                            println!("[Continuing...]");
+                            continue;
+                        }
+                        _ => {
+                            println!("[Breaking...]");
+                            break;
+                        }
+                    }
+                }
                 break;
+            }
+            Err(_) => {
+                eprintln!("[There was a problem with the connection]");
+                continue;
             }
         }
     }
+
+    
 }
 
 fn input(prompt: &str) -> io::Result<String> {
