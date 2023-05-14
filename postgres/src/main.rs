b1129835a112411f993500a08a7ee599ec4ab8b7
tomashrib <tomas.hrib@proton.me>
2023-05-14 21:38:21 +0200

    feat: get vals from user and insert a new record into db

--- postgres/src/main.rs
+++ postgres/src/main.rs
@@ -1,4 +1,5 @@
 use tokio_postgres::NoTls;
+use std::io::{self, Write};
 
 #[tokio::main]
 async fn main() {
@@ -14,15 +15,37 @@ async fn main() {
         }
     });
 
-    // Create table
-    client.execute(
-        "CREATE TABLE users (
-            id SERIAL PRIMARY KEY,
-            name VARCHAR NOT NULL,
-            email VARCHAR NOT NULL UNIQUE
-        )",
-        &[],
-    ).await.unwrap();
+    loop {
+        let name = input("Please enter name: ").unwrap();
+        let email = input("Please enter email: ").unwrap();
+    
+        // Insert a new user
+        client.execute(
+            "INSERT INTO users (name, email) VALUES ($1, $2)",
+            &[&name, &email],
+        ).await.unwrap();
+    
+        println!("Inserted new user");
 
-    println!("Table users created");
+        let add_another_user = input("Add another user? [y/n]: ").unwrap();
+
+        if add_another_user == "y" {
+            continue;
+        } else {
+            break;
+        }
+    }
+}
+
+fn input(prompt: &str) -> io::Result<String> {
+    print!("{}", prompt);
+    io::stdout().flush()?;
+
+    let mut line = String::new();
+    io::stdin().read_line(&mut line)?;
+
+    let len = line.trim_end_matches(&['\r', '\n'][..]).len();
+    line.truncate(len);
+
+    Ok(line)
 }
