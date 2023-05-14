3e49694d292bcae0ce9e294a15b0c1079054053b
tomashrib <tomas.hrib@proton.me>
2023-05-14 19:23:25 +0200

    feat: use method instead of function

--- structs_projects/rectangles/src/main.rs
+++ structs_projects/rectangles/src/main.rs
@@ -1,6 +1,13 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
 }
 
 fn main() {
@@ -9,12 +16,5 @@ fn main() {
         height: 50,
     };
 
-    println!(
-        "The area of rectangle is {} square pixels.",
-        area(&rect1)
-    );
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    println!("Area of rectangle is {} square px.", rect1.area());
 }
