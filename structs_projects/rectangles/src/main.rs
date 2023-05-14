493b969f42231d089d0a7984f63aeca60f4004aa
tomashrib <tomas.hrib@proton.me>
2023-05-14 19:01:31 +0200

    feat: refactor code to use structs

--- structs_projects/rectangles/src/main.rs
+++ structs_projects/rectangles/src/main.rs
@@ -1,13 +1,20 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
-    let width1 = 30;
-    let height1 = 50;
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
 
     println!(
-        "Area of rectangle is {} square pixels",
-        area(width1, height1)
+        "The area of rectangle is {} square pixels.",
+        area(&rect1)
     );
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
