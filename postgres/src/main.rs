ce88c6632c2b13dda2d0d0df1c0bd5a3ab410099
tomashrib <tomas.hrib@proton.me>
2023-05-26 18:47:01 +0200

    feat: handle login and insert user into db

--- postgres/src/main.rs
+++ postgres/src/main.rs
@@ -1,5 +1,5 @@
-use tokio_postgres::NoTls;
 use std::io::{self, Write};
+use tokio_postgres::NoTls;
 
 struct Login {
     username: String,
@@ -9,24 +9,26 @@ struct Login {
 
 #[tokio::main]
 async fn main() {
-
     println!("[Logging into PostgreSQL server]");
-    
+
     let mut login = Login {
         username: String::new(),
         password: String::new(),
         db_name: String::new(),
     };
-    
+            
     loop {
         login.username = input("Enter username: ").unwrap();
         login.password = rpassword::read_password_from_tty(Some("Enter password: ")).unwrap();
         login.db_name = input("Enter db name: ").unwrap();
-    
-        let login_credentials = format!("host=localhost user={} password={} dbname={}", &login.username, &login.password, &login.db_name);
-    
+
+        let login_credentials = format!(
+            "host=localhost user={} password={} dbname={}",
+            &login.username, &login.password, &login.db_name
+        );
+
         let connection_result = tokio_postgres::connect(&login_credentials, NoTls).await;
-        
+
         match connection_result {
             Ok((client, connection)) => {
                 tokio::spawn(async move {
@@ -34,19 +36,22 @@ async fn main() {
                         eprintln!("connection error: {}", e);
                     }
                 });
-                
+
                 println!("[Connected successfully to {}]", &login.db_name);
 
                 loop {
                     let name = input("Please enter name: ").unwrap();
                     let email = input("Please enter email: ").unwrap();
-                
+
                     // Insert a new user
-                    client.execute(
-                        "INSERT INTO users (name, email) VALUES ($1, $2)",
-                        &[&name, &email],
-                    ).await.unwrap();
-                
+                    client
+                        .execute(
+                            "INSERT INTO users (name, email) VALUES ($1, $2)",
+                            &[&name, &email],
+                        )
+                        .await
+                        .unwrap();
+
                     println!("[Inserted new user]");
 
                     let add_another_user = input("Add another user? [y/n]: ").unwrap();
@@ -70,8 +75,6 @@ async fn main() {
             }
         }
     }
-
-    
 }
 
 fn input(prompt: &str) -> io::Result<String> {
